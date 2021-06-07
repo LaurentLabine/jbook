c5e210711b82abdda7702d48f75d20d12be4aed6
Laurent Labine <laurentlabine@gmail.com>
2021-06-07 17:19:25 +0200

    Edited path to build dynamically

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,37 +1,51 @@
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
  
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args)
         if (args.path === 'index.js') {
-          return {path: args.path, namespace: 'a'}
-        } else if(args.path === 'tiny-text-pkg')
-        return { 
-          path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
-          namespace: 'a' };
-      });
+          return { path: args.path, namespace: 'a' }
+        }
+        
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+
+        
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   }
+        // }
+      })
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+        console.log('onLoad', args)
  
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
+          }
         }
-      });
+
+        const { data } = await axios.get(args.path)
+
+        return {
+          loader: 'jsx',
+          contents: data,
+        }
+      })
     },
-  };
-};
+  }
+}
