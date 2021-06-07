cfabb42e790abb159ae3a1ac0366f350dc911c3b
Laurent Labine <laurentlabine@gmail.com>
2021-06-07 21:01:31 +0200

    Replaced logic using  promises and regex and added comments to make code simpler

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,20 +10,21 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args)
+      // Handles Root entryu file of index.js
+      build.onResolve({filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a'}
+      })
 
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' }
+      // Handles relative paths in a module
+      build.onResolve({filter: /^\.+\//}, (args: any) => {
+        return {
+        namespace: 'a',
+        path: new URL(args.path, 'https://unpkg.com' + args.resolveDir +  '/').href,
         }
+      })
 
-        if(args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir +  '/').href,
-          };
-        }
-        
+      // Handles main file of a module
+      build.onResolve({filter: /.*/ }, async (args: any) => {       
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
