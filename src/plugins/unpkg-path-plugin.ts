0ac9687febc97fbe067f24577caa792ff873e137
Laurent Labine <laurentlabine@gmail.com>
2021-06-07 20:44:05 +0200

    implementing caching & resuse of fetched documents

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -51,15 +51,27 @@ export const unpkgPathPlugin = () => {
           }
         }
 
+        // Check to see if we already fetched this file and if it is in the cache.
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+
+        //  If it is, return it immediately.
+        if(cachedResult) return cachedResult
 
 
         const { data, request } = await axios.get(args.path)
+        //Store response in cache
 
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
+
+        // Otherwise, store in cache.
+        await fileCache.setItem(args.path, result)
+
+        return result
       })
     },
   }
