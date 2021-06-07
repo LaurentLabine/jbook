70a1a68213c7935f6128918d980de886c020f4af
Laurent Labine <laurentlabine@gmail.com>
2021-06-07 18:19:39 +0200

    Fix the nested package bundle

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,7 +15,7 @@ export const unpkgPathPlugin = () => {
         if(args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href,
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir +  '/').href,
           };
         }
         
@@ -32,17 +32,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           }
         }
 
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
 
+        console.log(request)
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
       })
     },
