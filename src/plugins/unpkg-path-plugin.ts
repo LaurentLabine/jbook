5c8e0f4276e40a0b8e17b50f5738ce822e2d21c0
Laurent Labine <laurentlabine@gmail.com>
2021-06-07 17:59:06 +0200

    Dynamically construct import paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -7,23 +7,22 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args)
+
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' }
+        }
+
+        if(args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
         }
         
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         }
-
-        
-
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return {
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //     namespace: 'a',
-        //   }
-        // }
       })
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -33,7 +32,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           }
