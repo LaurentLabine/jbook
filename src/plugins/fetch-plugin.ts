2b36d4e84a4537b26ea7bb78accc4af1c42e64a5
Laurent Labine <laurentlabine@gmail.com>
2021-06-08 10:07:44 +0200

    Escaping the special characters and feeding the css to the content.

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -32,11 +32,17 @@ export const fetchPlugin = (inputCode: string) => {
 
 
       const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
+
+      const escaped = data
+      .replace(/\n/g, '')
+      .replace(/"/g, '\\"')
+      .replace(/'/g, "\\'")
+
       const contents = 
       fileType === 'css'
       ? `
       const style = document.createElement('style');
-      style.innerText = 'body { background-color: "red"}';
+      style.innerText = '${escaped}';
       document.head.appendChild(style);
       `
         : data;
