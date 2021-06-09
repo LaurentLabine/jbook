24eda5bde6e40b7cd87d16c67a7d86199e151ed5
Laurent Labine <laurentlabine@gmail.com>
2021-06-09 15:17:05 +0200

    Configured window resizing effect

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -10,6 +10,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
 
   useEffect(() => {
     let timer: any;
@@ -21,6 +22,9 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       setTimeout(() => {
         setInnerHeight(window.innerHeight);
         setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
       }, 100);
     };
     window.addEventListener('resize', listener);
@@ -28,7 +32,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     return () => {
       window.removeEventListener('resize', listener);
     };
-  }, []);
+  });
 
   switch (direction) {
     case 'horizontal':
@@ -37,8 +41,11 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
         minConstraints: [innerWidth * 0.2, Infinity],
         maxConstraints: [innerWidth * 0.75, Infinity],
         height: Infinity,
-        width: innerWidth * 0.75,
+        width: width,
         resizeHandles: ['e'],
+        onResizeStop: (event, data) => {
+          setWidth(data.size.width);
+        },
       };
       break;
     default:
