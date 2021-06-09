3f303ea0712a64bcd04e4f82473779371f80579f
Laurent Labine <laurentlabine@gmail.com>
2021-06-09 14:37:50 +0200

    Added styling to make horizontal draggable an option.

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,16 +1,35 @@
 import './resizable.css';
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  switch (direction) {
+    case 'horizontal':
+      resizableProps = {
+        className: 'resize-horizontal',
+        minConstraints: [window.innerWidth * 0.2, Infinity],
+        maxConstraints: [window.innerWidth * 0.75, Infinity],
+        height: Infinity,
+        width: window.innerWidth * 0.75,
+        resizeHandles: ['e'],
+      };
+      break;
+    default:
+      resizableProps = {
+        maxConstraints: [Infinity, window.innerHeight * 0.9],
+        minConstraints: [Infinity, 24],
+        height: direction === 'vertical' ? 300 : Infinity,
+        width: Infinity,
+        resizeHandles: ['s'],
+      };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
