b10c117a3b5c705c97ce2fdd4932d7eeb4355a48
Laurent Labine <laurentlabine@gmail.com>
2021-06-10 23:47:41 +0200

    Fixed typo

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -4,11 +4,11 @@ import CodeCell from './code-cell';
 import TextEditor from './text-editor';
 import ActionBar from './action-bar';
 
-interface CelllistItemProps {
+interface CellListItemProps {
   cell: Cell;
 }
 
-const CellListItem: React.FC<CelllistItemProps> = ({ cell }) => {
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
     child = (
@@ -30,4 +30,5 @@ const CellListItem: React.FC<CelllistItemProps> = ({ cell }) => {
 
   return <div className="cell-list-item">{child}</div>;
 };
+
 export default CellListItem;
