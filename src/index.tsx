38bdbcdcbcc301af7b6dd72dc1537824cb4b675b
Laurent Labine <laurentlabine@gmail.com>
2021-06-10 14:51:36 +0200

    Removed unused components to remove warnings

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
-// import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
