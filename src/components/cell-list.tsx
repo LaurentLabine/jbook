8bebb84a6a2b5e4ad7d3f5a92bbbd246c0fdfb89
Laurent Labine <laurentlabine@gmail.com>
2021-06-10 22:18:01 +0200

    Styled the add text and code buttons and forced em visible if all prompts deleted

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,13 +1,25 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id: string) => data[id])
   );
 
-  const renderedCells = cells.map((cell) => <CellListItem cell={cell} />);
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
+  ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 };
 export default CellList;
