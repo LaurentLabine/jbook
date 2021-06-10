f6c46bf4929c2c1dbaef7a88825d8f4d4b3178eb
Laurent Labine <laurentlabine@gmail.com>
2021-06-10 11:55:24 +0200

    Implemented cellsReducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -24,19 +24,51 @@ const reducer = produce(
     switch (action.type) {
       case ActionType.UPDATE_CELL:
         const { id, content } = action.payload;
-
         state.data[id].content = content;
         return;
       case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
         return state;
       case ActionType.MOVE_CELL:
-        return state;
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) return; // Invalid state update
+
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return;
       case ActionType.INSERT_CELL_BEFORE:
-        return state;
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+
+        state.data[cell.id] = cell;
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+
       default:
         return state;
     }
   }
 );
+
+// Random ID generation function
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
+};
 
 export default reducer;
