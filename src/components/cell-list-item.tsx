1d0a2d09e1be1b231bf9a61ca413cfccbfabc86a
Laurent Labine <laurentlabine@gmail.com>
2021-06-10 15:44:48 +0200

    implemented and imported new components

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,19 @@
-const CellListItem: React.FC = () => {};
+import { Cell } from '../state';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
 
+interface CelllistItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CelllistItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
+};
 export default CellListItem;
--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,13 @@
-const CellList: React.FC = () => {};
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
 
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
+
+  const renderedCells = cells.map((cell) => <CellListItem cell={cell} />);
+
+  return <div>{renderedCells}</div>;
+};
 export default CellList;
