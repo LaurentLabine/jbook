d8ab81ae169835df032ed9d1d9b89942b44fb943
Laurent Labine <laurentlabine@gmail.com>
2021-07-01 16:28:56 +0200

    Refactored logic and bundled

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,28 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { ActionType } from './action-types';
+import { persistMiddlware } from './middlewares/persistmiddlware';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-// Manually testing store.
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: { id: null, type: 'code' },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: { id: null, type: 'text' },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: { id: null, type: 'code' },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: { id: null, type: 'text' },
-});
-// console.log(store.getState());
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(persistMiddlware, thunk)
+);
